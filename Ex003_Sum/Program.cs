cc7d2e1ad5de7b13597a34c5bb63aaaca1b117fb
Elena Gladysheva <gladen000@gmail.com>
2022-10-15 18:14:12 +0300

    fixed the bug in random

--- Ex003_Sum/Program.cs
+++ Ex003_Sum/Program.cs
@@ -1,4 +1,4 @@
-﻿int numberA = new Random().Next(0,9); // namber 1-9
+﻿int numberA = new Random().Next(1,10); // namber 1-9
 Console.WriteLine(numberA);
 int numberB = new Random().Next(0,9);
 Console.WriteLine(numberB);

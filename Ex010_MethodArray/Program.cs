330d3707f8b831b79c8a71ca7f4c2fc4cd7ce390
Elena Gladysheva <gladen000@gmail.com>
2022-10-18 11:55:31 +0300

    Added Ex010_MethodArray

---
+++ Ex010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.Clear();
+
+int[] array = {1,55,44,2,99,45,99,56};
+int n = array.Length;
+int index = 0;
+int find = 99;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; 
+    }
+    index++;
+}

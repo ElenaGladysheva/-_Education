ecf95ca1b812852e391d2d97b729e8a739b7d180
Elena Gladysheva <gladen000@gmail.com>
2022-10-18 10:45:42 +0300

    Function max of 9 number added

--- Ex008_MaxFun/Program.cs
+++ Ex008_MaxFun/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int MuxFun (int arg1, int arg2, int arg3)
+{   
+    Console.Clear();
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+    
+}
+
+int a1 = 54;
+int b1 = 4;
+int c1 = 89;
+int a2 = 56;
+int b2 = 1111;
+int c2 = 10000;
+int a3 = 0;
+int b3 = -102;
+int c3 = 29;
+
+// int max1 = MuxFun(a1,b1,c1);
+// int max2 = MuxFun(a2,b2,c2);
+// int max3 = MuxFun(a3,b3,c3);
+// int max = MuxFun (max1, max2, max3);
+
+int max = MuxFun (MuxFun(a1,b1,c1),
+                  MuxFun(a2,b2,c2), 
+                  MuxFun(a3,b3,c3));
+
+Console.WriteLine(max);
+

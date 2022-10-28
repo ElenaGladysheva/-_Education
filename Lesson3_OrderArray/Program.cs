938d99aa7d298d543c04f47ba054a15e55894a5c
Elena Gladysheva <gladen000@gmail.com>
2022-10-28 16:55:45 +0300

    Task 2 solved.

--- Lesson3_OrderArray/Program.cs
+++ Lesson3_OrderArray/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 2. Написать программу сортировки - прямой выбор.
+int [] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+void PrintArray (int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length -1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i+1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition])
+                minPosition = j;
+        }
+
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+Console.Clear();
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
+

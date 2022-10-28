295acb1a59756cbc073bd5756d8cce4d0a25ff32
Elena Gladysheva <gladen000@gmail.com>
2022-10-28 14:30:38 +0300

    Lesson 3, theoretical part .

--- Lesson3_Method1/Program.cs
+++ Lesson3_Method1/Program.cs
@@ -1,2 +1,95 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//вид 1. (Метод без входных и выходных данных)
+/*
+void Method1()
+{
+    Console.WriteLine("Автор...");
+}
+Method1();
+*/
+//вид 2. (На вход принимаем, а на выход переменных нет.)
+/*
+void Method2(string msg)
+{
+   Console.WriteLine(msg); 
+}
+ Method2("Текс");
+*/
+/*
+ void Method21(string msg, int count)
+{  
+    int i = 0;
+    while(i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }  
+}
+Method21(count: 4, msg: "Текст");
+*/
+
+
+//Вид 3. (Ничего не принимают, возвращают переменную.)
+/*
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+
+int year = Method3();
+Console.WriteLine(year);
+*/
+
+
+//Вид 4. (Есть переменные и на вход и навыход)
+/*
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+*/
+
+
+// Цикл for
+/*
+string Method41(int count, string text)
+{
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+
+}
+
+ string res = Method41(10, "z");
+ Console.WriteLine(res);
+ */
+
+
+ //Цикл в цикле
+
+for (int i = 2; i <=10; i++)
+{
+    for (int j = 2; j <=10; j++)
+    {
+        Console.WriteLine($"{i}x{j} = {i*j}");
+    }
+    Console.WriteLine();
+}
+   
+
+
+
+
+
+
+
+
